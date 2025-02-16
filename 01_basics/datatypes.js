b0b1583049bd1898f72d84b98ed7c3c94c6260ec
Swati8125 <121600422+Swati8125@users.noreply.github.com>
2025-02-16 14:33:02 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict";// Treat all JS code as newer version.
+
+//alert(3+3) //On browser it will work but we are using node js so, it will not work right now.
+
+console.log(3+
+    3); // code readability should be high.
+
+/* DATATYPES IN JS
+
+
+number=> Range is 2 to the power 53
+bigint=> for large data or numbers we use
+string
+boolean=> true/fasle
+null=> a standalone value (It represents an empty value.)
+undefined=> value not defined
+symbol=> for finding the uniqueness we use symbol.
+object=>  
+typeof()=>
+*/
+
+let age=23;
+console.log(typeof age)
+
+/* INTERVIEW QUESTION
+console.log(typeof null) //Returns an object
+console.log(typeof undefined); // Returns undefined 
+*/
